a34bc340e6e94c10f2ec1954fc98f22608dd7349
Chris Ritter <thechrisritter@gmail.com>
2014-09-25 13:43:43 -0500

    Create adding_lists.py

---
+++ code/adding_lists.py
@@ -0,0 +1,21 @@
+# A simple code I used to collect simple data from thosands of items in two lists.
+a = [1, 2, 3, 4]
+b = [2, 3, 7, 8]
+
+c = set(b) - set(a)
+d = set(a) - set(b)
+e = a + b
+
+total = len(e)
+matches = len(d)
+unique = len(c)
+a_total = len(a)
+b_total = len(b)
+print('Results:')
+print(c)
+print('List A total: ', a_total)
+print('List B total: ', b_total)
+print('Total: ', total)
+print('Matches: ', matches)
+print('Unique items: ', unique)
+input()
